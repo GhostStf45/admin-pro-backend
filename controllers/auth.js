fc642f689591bab9caf918c3bcd0ee02003a8c08
unknown <topguncounter@hotmail.com>
2021-02-01 09:30:07 -0500

    Fin seccion 18

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,6 +3,7 @@ const bcrypt = require('bcryptjs');
 const Usuario = require('../models/usuario');
 const { generarJWT } = require('../helpers/jwt');
 const { googleVerify } = require('../helpers/google-verify');
+const { getMenuFrontEnd } = require('../helpers/menu-frontend');
 
 
 
@@ -48,8 +49,9 @@ const login = async(req, res = response) => {
         res.json({
             ok:true,
             usuario:usuarioDB,
-            token
-        })
+            token,
+            menu: getMenuFrontEnd( usuarioDB.role )
+        });
     } catch (error) {
         console.log(error);
         res.status(500).json(
@@ -91,7 +93,7 @@ const googleSignIn = async (req, res = response) => {
       res.json({
           ok: true,
           token,
-          usuario: usuarioDB
+          menu: getMenuFrontEnd(usuario.role)
       })
     } catch (error) {
         res.status(401).json({
@@ -115,7 +117,8 @@ const renewToken = async (req, res = response)=> {
     res.json ({
         ok: true,
         usuario: usuarioDB,
-        token
+        token,
+        menu: getMenuFrontEnd( usuarioDB.role )
     })
 }
 
