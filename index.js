0da052930758960bcd259297a79f5d8ded9015f1
unknown <topguncounter@hotmail.com>
2021-02-01 11:04:43 -0500

    SPA control

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 //leer Variables de entorno
 require('dotenv').config();
+
+const path = require('path');
+
 const express = require('express');
 const cors = require('cors');
 const { dbConnection } = require('./database/config');
@@ -25,6 +28,13 @@ app.use('/api/medicos', require('./routes/medicos'));
 app.use('/api/todo', require('./routes/busquedas'));
 app.use('/api/login', require('./routes/auth'));
 app.use('/api/uploads', require('./routes/uploads'));
+
+
+// Lo ultimo
+
+app.get('*', (req,res) => {
+    res.sendFile( path.resolve(__dirname,'public/index.html'));
+});
 
 app.listen( process.env.PORT, ()=>{
     console.log('Servidor corriendo en puerto ' + process.env.PORT);
